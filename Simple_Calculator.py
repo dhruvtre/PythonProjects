31a8ad90344db7f2ad4c9228b412eb7d74d00c70
dhruvtre <59575803+dhruvtre@users.noreply.github.com>
2023-06-24 20:40:56 +0530

    Comments Simple_Calculator.py

--- Simple_Calculator.py
+++ Simple_Calculator.py
@@ -1,12 +1,16 @@
+#defining an addition function
 def add(a, b):
     print(f"Adding {a} and {b}")
     return a + b
+#defining an subtraction function
 def subtract(a, b):
     print(f"Subtracting {a} and {b}")
     return a - b
+#defining an multiplication function
 def multiply(a, b): 
     print(f"Multiplying {a} and {b}")
     return a * b
+#defining an division function
 def divide(a, b): 
     print(f"Dividing {a} and {b}...")
     return a / b
@@ -16,9 +20,12 @@ print("Let's do some math with just functions")
 #taking user input for the numbers
 number1 = int(input("What is the first number? \n >"))
 number2 = int(input("What is the second number? \n >"))
+
+#implementing different calculation functions on user input
 add_result = add(number1, number2)
 subtract_result = subtract(number1, number2)
 multiply_result = multiply(number1, number2)
 divide_result = divide(number1, number2)
 
+#printing all results using escapes
 print(f"N1: {number1}, N2: {number2}, \n\t Addition: {add_result}, \n\t Subtraction: {subtract_result}, \n\t Multiplication: {multiply_result}, \n\t Division: {divide_result}.")
